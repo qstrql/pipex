669f2d91379e36e558bfa5bb7b2db0306981b347
mjouot <mjouot@student.42angouleme.fr>
2022-11-22 17:17:02 +0100

    aa

--- mandatory/pipex.c
+++ mandatory/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mjouot <mjouot@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 16:15:30 by mjouot            #+#    #+#             */
-/*   Updated: 2022/11/22 17:05:07 by mjouot           ###   ########.fr       */
+/*   Updated: 2022/11/22 17:16:57 by mjouot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 int	main(int argc, char **argv, char **envp)
 {
 	int	fd[2];
+	int	pipefd[2];
 
 	if (argc != 5)
 		ft_error("Wrong number of argument");
@@ -24,10 +25,12 @@ int	main(int argc, char **argv, char **envp)
 		ft_are_args_ok(argc, argv);
 	fd[0] = open(argv[1], O_RDONLY);
 	if (fd[0] < 0)
-		ft_error("fd error");
-	fd[1] = open(argv[4], O_RDONLY);
+		ft_error(argv[1]);
+	fd[1] = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd[1] < 0)
-		ft_error("fd error");
+		ft_error(argv[4]); //argv[argc - 1];
+	if (pipe(pipefd) < 0)
+		ft_error("pipe error");
 	
 	return (0);
 }
